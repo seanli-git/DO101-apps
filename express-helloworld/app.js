e542583494c994db7ef2bb7c7d51aacb3352c723
seanli-git <xqli07746@yahoo.com>
2020-06-29 16:22:42 -0400

    add endpoint /echo

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -8,6 +8,11 @@ app.get('/', function (req, res) {
 app.get("/mars", function(req, res) {
   res.send('Hello Sean!\n')
 })
+
+app.get("/echo", function(req, res) {
+  res.send('application inforamtion will be ready soon!\n')
+})
+
 app.listen(8090, function () {
   console.log('Example app listening on port 8090!');
 });
